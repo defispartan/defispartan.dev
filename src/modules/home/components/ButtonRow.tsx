ba6b982ec952225df0b329bc8dd8da26983fec26
aschmidt20 <andrew@aave.com>
2023-04-24 16:25:16 -0500

    chore: shift directories and optimize loading

--- src/modules/home/components/ButtonRow.tsx
+++ src/modules/home/components/ButtonRow.tsx
@@ -16,35 +16,35 @@ const ButtonRow = () => {
                 onClick={() => handleClick('https://github.com/defispartan')}
                 className={`${styles.iconButton} ${styles.github}`}
             >
-                <Image src='/github.svg' alt='GitHub' width={32} height={32} />
+                <Image src='/github.svg' alt='GitHub' width={32} height={32} className={styles.iconButtonImage} />
             </button>
 
             <button
                 onClick={() => handleClick('https://twitter.com/defispartan')}
                 className={`${styles.iconButton} ${styles.twitter}`}
             >
-                <Image src='/twitter.svg' alt='Twitter' width={32} height={32} />
+                <Image src='/twitter.svg' alt='Twitter' width={32} height={32} className={styles.iconButtonImage} />
             </button>
 
             <button
                 onClick={() => handleClick('https://lenster.xyz/u/defispartan')}
                 className={`${styles.iconButton} ${styles.lens}`}
             >
-                <Image src='/lens.svg' alt='Twitter' width={40} height={40} />
+                <Image src='/lens.svg' alt='Lens' width={32} height={32} className={styles.iconButtonImage} />
             </button>
 
             <button
                 onClick={() => handleClick('https://www.youtube.com/@defispartan')}
                 className={`${styles.iconButton} ${styles.youtube}`}
             >
-                <Image src='/youtube.svg' alt='Twitter' width={50} height={50} />
+                <Image src='/youtube.svg' alt='YouTube' width={32} height={32} className={styles.iconButtonImage} />
             </button>
 
             <button
                 onClick={() => handleClick('mailto:defispartan@gmail.com')}
                 className={`${styles.iconButton} ${styles.email}`}
             >
-                <Image src='/email.svg' alt='Email' width={24} height={16} />
+                <Image src='/email.svg' alt='Email' width={32} height={32} className={styles.iconButtonImage} />
             </button>
         </div>
     );
