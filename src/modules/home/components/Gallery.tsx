ac4b1010a709dded8c71f48e533ad637d293b5bb
defispartan <defispartan@gmail.com>
2024-01-28 23:07:41 -0600

    feat: home update (#9)
    * feat: bun :)
    
    * feat: README
    
    * feat: update gallery row

--- src/modules/home/components/Gallery.tsx
+++ src/modules/home/components/Gallery.tsx
@@ -1,45 +1,84 @@
 import React, { useState } from "react";
 import styles from "../../../styles/Home.module.css";
-import Image from "next/image";
 import { useRouter } from "next/router";
+
+interface Button {
+  title: string;
+  description: string;
+  link: string;
+}
 
 const Gallery = () => {
   const router = useRouter();
-
-  const buttonsData = [
+  const buttonsData: Button[] = [
+    {
+      title: "createweb3ui.xyz",
+      description: "Fast and simple Web3 frontend template",
+      link: "https://createweb3ui.xyz",
+    },
+    {
+      title: "Aave Flashloan Demo",
+      description: "Starter contracts and development guide",
+      link: "https://github.com/defispartan/testnet-flash-liquidity",
+    },
+    {
+      title: "Fullstack Starter Kit",
+      description: "Frontend + backend template powered by PocketBase",
+      link: "https://github.com/defispartan/fullstack-starter-kit",
+    },
+    {
+      title: "Hello World Open Action",
+      description: "Contract and UI template for Lens open action",
+      link: "https://github.com/defispartan/hello-world-open-action",
+    },
     {
       title: "mylenny.xyz",
-      description: "Tool to view Lens Profile NFT image",
+      description: "View Lens Profile NFT image",
+      link: "https://mylenny.xyz",
     },
   ];
 
-  const [hoveredDescriptions, setHoveredDescriptions] = useState(
-    Array(buttonsData.length).fill("")
-  );
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
 
   const handleClick = (url: string) => {
     router.prefetch(url);
     window.open(url, "_blank");
   };
 
-  const handleButtonHover = (index: number, description: string) => {
-    const updatedHoveredDescriptions = [...hoveredDescriptions];
-    updatedHoveredDescriptions[index] = description;
-    setHoveredDescriptions(updatedHoveredDescriptions);
+  const handleButtonHover = (index: number) => {
+    setHoveredIndex(index);
   };
 
+  const handleButtonLeave = () => {
+    setHoveredIndex(-1);
+  };
+
+  const numRows = Math.ceil(buttonsData.length / 3);
+
   return (
-    <div className={styles.galleryRow}>
-      {buttonsData.map((button, index) => (
-        <button
-          key={index}
-          onClick={() => handleClick("https://mylenny.xyz")}
-          className={`${styles.iconButton} ${styles.galleryButton}`}
-          onMouseEnter={() => handleButtonHover(index, button.description)}
-          onMouseLeave={() => handleButtonHover(index, "")}
-        >
-          <p>{hoveredDescriptions[index] || button.title}</p>
-        </button>
+    <div className={styles.galleryContainer}>
+      {Array.from({ length: numRows }).map((_, rowIndex) => (
+        <div key={rowIndex} className={styles.galleryRow}>
+          {buttonsData
+            .slice(rowIndex * 3, (rowIndex + 1) * 3)
+            .map((button, index) => (
+              <button
+                key={index}
+                onClick={() => handleClick(button.link)}
+                className={`${styles.iconButton} ${styles.galleryButton} ${
+                  hoveredIndex === rowIndex * 3 + index ? styles.hovered : ""
+                }`}
+                onMouseEnter={() => handleButtonHover(rowIndex * 3 + index)}
+                onMouseLeave={handleButtonLeave}
+              >
+                <p>
+                  {hoveredIndex === rowIndex * 3 + index
+                    ? button.description
+                    : button.title}
+                </p>
+              </button>
+            ))}
+        </div>
       ))}
     </div>
   );
