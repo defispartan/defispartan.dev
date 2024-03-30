bdb54fc9e6bc1e048dc223ccac303524dff46fb6
aschmidt20 <andrew@aave.com>
2024-03-30 02:09:30 -0500

    feat: persist background between screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import "../src/styles/globals.css";
+import "@/styles/globals.css";
+import styles from "@/styles/Home.module.css";
 import type { AppProps } from "next/app";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const placeholderSvg =
+    "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxIDEiIGZpbGw9IiMxMjEyMTIiPjwvc3ZnPg==";
+
+  return (
+    <div className={styles.cover}>
+      <div className={styles.background} />
+      <Image
+        src="/bg.webp"
+        fill
+        style={{ objectFit: "cover" }}
+        alt="Background"
+        priority
+        placeholder="blur"
+        blurDataURL={placeholderSvg}
+      />
+      <Component {...pageProps} />
+    </div>
+  );
 }
